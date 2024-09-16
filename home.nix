59a421f4d86465c36f4f020a8481e0d2b8e44b8a
Berry Phillips <berryphillips@gmail.com>
2024-09-16 21:39:58 +0900

    Add home-manager

--- home.nix
+++ home.nix
@@ -33,6 +33,13 @@ home.preferXdgDirectories = true;
   programs.direnv.nix-direnv.enable = true;
 
   programs.ssh.enable = true;
+  programs.ssh.matchBlocks = {
+    "*" = {
+      extraOptions = {
+        "IdentityAgent" = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";
+      };
+     };
+  };
 
   programs.zoxide.enable = true;
 
